d453539cdeafb1e941557253bf7b5aaa7e069332
Matt Wilkin and Sam Park <sam@>
2016-06-27 16:24:53 -0700

    First spec success

--- ping_pong/lib/ping_pong.rb
+++ ping_pong/lib/ping_pong.rb
@@ -1,5 +1,27 @@
 class Fixnum
   define_method(:ping_pong) do
-    input = (1..self)
+    # number_array = []
+    input = (1..self).to_a
+    # if(self % 3 == 0)
+    #   number_array.push("ping")
+    # end
+    # number_array.push(1..self)
   end
 end
+
+#   define_method(:ping_pong) do
+#     number_array = []
+#     index = 1
+#     self.times() do
+#      number_array.push(index)
+#      index += 1
+#     end
+#     number_array.each_index do |num|
+#       if(number_array[num] % 3 == 0)
+#         number_array.push("ping")
+#       end
+#     end
+#     number_array
+#   end
+# end
+#
