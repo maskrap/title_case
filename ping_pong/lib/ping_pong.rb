62a2eb8d8d43d8e9caecd0fb8620650d5c7aa1e0
Matt Wilkin and Sam Park <matt@>
2016-06-27 16:47:14 -0700

    Final spec completed. Go us.

--- ping_pong/lib/ping_pong.rb
+++ ping_pong/lib/ping_pong.rb
@@ -3,8 +3,14 @@ class Fixnum
     number_array = []
     input = (1..self).to_a
     input.each() do |number|
-      if number.%(3) == 0
-        number_array.push("ping")
+      if number.%(15) == 0
+        number_array.push("pingpong")
+      elsif  
+        number.%(5) == 0
+        number_array.push("pong")
+      elsif
+        number.%(3) == 0
+          number_array.push("ping")
       else
         number_array.push(number)
       end
@@ -12,20 +18,3 @@ class Fixnum
     number_array
   end
 end
-
-#   define_method(:ping_pong) do
-#     number_array = []
-#     index = 1
-#     self.times() do
-#      number_array.push(index)
-#      index += 1
-#     end
-#     number_array.each_index do |num|
-#       if(number_array[num] % 3 == 0)
-#         number_array.push("ping")
-#       end
-#     end
-#     number_array
-#   end
-# end
-#
